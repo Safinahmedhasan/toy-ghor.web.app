c147b68f2ed29882b74021c4abe1d44956fc3242
safinahmedhasan <safinahmedhaasan@gmail.com>
2023-05-18 23:40:22 +0600

    tab added

--- src/component/ShopByCatagory/ShopByCatagory.jsx
+++ src/component/ShopByCatagory/ShopByCatagory.jsx
@@ -1,38 +1,51 @@
-import { Tabs } from "flowbite-react";
-import { useEffect, useState } from "react";
+import { Card } from "flowbite-react";
+import { useState, useEffect } from "react";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 
 
 const ShopByCatagory = () => {
     const [carData, setCarData] = useState([]);
-    
-    console.log(carData);
 
     useEffect(() => {
-        fetch('car.json')
-          .then((response) => response.json())
-          .then((data) => setCarData(data));
-      }, []);
+        fetch("carTab.json")
+            .then((response) => response.json())
+            .then((data) => setCarData(data))
+            .catch((error) => console.log("Error fetching car data:", error));
+    }, []);
 
 
     return (
-        <div className="container mx-auto mt-20">
-            <h2 className='text-4xl hexfont text-center mt-16 mb-10 text-blue-600 '>Shop by category</h2>
-            <div className="w-7/12 mx-auto">
-                <Tabs.Group
-                    aria-label="Full width tabs"
-                    style="fullWidth"
-                >
-                    <Tabs.Item title="Sports Car">
-                        Profile content
-                    </Tabs.Item>
-                    <Tabs.Item title="Truck">
-                        Dashboard content
-                    </Tabs.Item>
-                    <Tabs.Item title="Police Car">
-                        Settings content
-                    </Tabs.Item>
-                </Tabs.Group>
-            </div>
+        <div className="container mx-auto">
+            <h2>Toy Catagory</h2>
+            <Tabs>
+                <div className="text-center">
+                    <TabList>
+                        <Tab>Tab 1</Tab>
+                        <Tab>Tab 2</Tab>
+                        <Tab>Tab 3</Tab>
+                    </TabList>
+                </div>
+
+                {Object.keys(carData).map((tabKey, tabIndex) => (
+                    <TabPanel key={tabIndex}>
+                        <div className="flex justify-center gap-10">
+                            {carData[tabKey].map((car, carIndex) => (
+                                <div key={carIndex}>
+                                    <div className="max-w-sm">
+                                        <Card>
+                                            <img className="w-96" src={car.image} alt={car.name} />
+                                            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{car.name}</h5>
+                                            <p>Price: {car.price}</p>
+                                            <p>Rating: {car.rating}</p>
+                                        </Card>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    </TabPanel>
+                ))}
+            </Tabs>
         </div>
     );
 };
